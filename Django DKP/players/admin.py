f80d1a17cc21783b5dbb0d5a749ea5907be8dbd8
Kyle Serio <k67serio54@gmail.com>
2022-08-10 17:42:03 -0700

    Delete Django DKP directory
    Now located in base repository, not main/

--- Django DKP/players/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Player
-from .models import Item
-from .models import File
-
-admin.site.register(Player)
-admin.site.register(Item)
-admin.site.register(File)
